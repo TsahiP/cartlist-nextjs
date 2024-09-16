bf82a5269c0df9575f64197ee2954c28d7506edd
tsahi pahima <tsahi013@gmail.com>
2024-09-16 16:40:38 +0300

    .

--- src/lib/authConfig.config.ts
+++ src/lib/authConfig.config.ts
@@ -30,7 +30,7 @@ export const authConfig: {
         },
         async authorized({ auth, request }) {
             const user = auth?.user;
-            const isOnLoginPage = request.nextUrl?.pathname.startsWith("/login") || request.nextUrl?.pathname.startsWith("/register")|| request.nextUrl?.pathname.startsWith("/");
+            const isOnLoginPage = request.nextUrl?.pathname.startsWith("/login") || request.nextUrl?.pathname.startsWith("/register");
             const isOnCartPage = request.nextUrl?.pathname.startsWith("/cart");
             const isOnCartsPage = request.nextUrl?.pathname.startsWith("/carts");
 
