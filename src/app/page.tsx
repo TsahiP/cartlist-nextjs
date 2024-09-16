f9b7586a1f8c4739940db90acf1c9f5bd55d7099
tsahi pahima <tsahi013@gmail.com>
2024-09-16 16:32:57 +0300

    .

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,40 +7,40 @@ import Login from "./(auth)/login/page";
 export default async function Home() {
   const session = await auth();
   return (
-    <><Login/></>
+    // <><Login/></>
 
-    // <div className="text-foreground bg-background  flex flex-col justify-center text-center">
-    //   <div className="flex items-center flex-col justify-center">
-    //     {/* <h1 className="text-primary text-3xl md:text-4xl font-bold mb-6">
+    <div className="text-foreground bg-background  flex flex-col justify-center text-center">
+      <div className="flex items-center flex-col justify-center">
+        {/* <h1 className="text-primary text-3xl md:text-4xl font-bold mb-6">
 
-    //     </h1> */}
-    //     <Image src="/logo.svg" alt="" width={200} height={200} />
-    //   </div>
-    //   {!session ?
-    //     <div className="flex gap-20 justify-center">
-    //       <div>
-    //         <Button asChild className="bg-primary  text-card-foreground shadow-md">
-    //           <Link href="/login" className="text-white">
-    //             Login
-    //           </Link>
-    //         </Button>
-    //       </div>
-    //       <div>
-    //         <Button asChild className="bg-accent text-accent-foreground shadow-md">
-    //           <Link href="/register">Register</Link>
-    //         </Button>
-    //       </div>
-    //     </div>
+        </h1> */}
+        <Image src="/logo.svg" alt="" width={200} height={200} />
+      </div>
+      {!session ?
+        <div className="flex gap-20 justify-center">
+          <div>
+            <Button asChild className="bg-primary  text-card-foreground shadow-md">
+              <Link href="/login" className="text-white">
+                Login
+              </Link>
+            </Button>
+          </div>
+          <div>
+            <Button asChild className="bg-accent text-accent-foreground shadow-md">
+              <Link href="/register">Register</Link>
+            </Button>
+          </div>
+        </div>
 
-    //   :
-    //     <div className="flex justify-center items-center flex-col ">
-    //       <Button asChild className="bg-accent text-accent-foreground w-20 shadow-md">
-    //         <Link href="/carts">לרשימות</Link>
-    //       </Button>
+      :
+        <div className="flex justify-center items-center flex-col ">
+          <Button asChild className="bg-accent text-accent-foreground w-20 shadow-md">
+            <Link href="/carts">לרשימות</Link>
+          </Button>
 
-    //     </div>
+        </div>
 
-    //   }
-    // </div>
+      }
+    </div>
   );
 }
