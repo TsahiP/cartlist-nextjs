fc7badfa36549236b40ed641144ff37cbbb4c590
tsahi pahima <tsahi013@gmail.com>
2024-09-16 15:57:11 +0300

    login bug fix

--- src/lib/authConfig.config.ts
+++ src/lib/authConfig.config.ts
@@ -1,58 +1,61 @@
 export const authConfig: {
-    pages: {
-        signIn: string;
-    };
-    providers: any[];
-    callbacks: {
-        jwt: (params: { token: any; user: any }) => Promise<any>;
-        session: (params: { session: any; token: any }) => Promise<any>;
-        authorized: (params: { auth: any; request: any }) => Promise<boolean | Response>;
-    };
+  pages: {
+    signIn: string;
+  };
+  providers: any[];
+  callbacks: {
+    jwt: (params: { token: any; user: any }) => Promise<any>;
+    session: (params: { session: any; token: any }) => Promise<any>;
+    authorized: (params: {
+      auth: any;
+      request: any;
+    }) => Promise<boolean | Response>;
+  };
 } = {
-    pages: {
-        signIn: "/login",
+  pages: {
+    signIn: "/login",
+  },
+  providers: [],
+  callbacks: {
+    async jwt({ token, user }) {
+      if (user) {
+        token.id = user._id;
+        token.isAdmin = user.isAdmin;
+      }
+      return token;
     },
-    providers: [],
-    callbacks: {
-        async jwt({ token, user }) {
-            if (user) {
-                token.id = user._id;
-                token.isAdmin = user.isAdmin;
-            }
-            return token;
-        },
-        async session({ session, token }) {
-            if (token) {
-                session.user.id = token.id;
-                session.user.isAdmin = token.isAdmin;
-            }
-            return session;
-        },
-        async authorized({ auth, request }) {
-            const user = auth?.user;
-            const isOnLoginPage = request.nextUrl?.pathname.startsWith("/login") || request.nextUrl?.pathname.startsWith("/register");
-            const isOnCartPage = request.nextUrl?.pathname.startsWith("/cart");
-            const isOnCartsPage = request.nextUrl?.pathname.startsWith("/carts");
+    async session({ session, token }) {
+      if (token) {
+        session.user.id = token.id;
+        session.user.isAdmin = token.isAdmin;
+      }
+      return session;
+    },
+    async authorized({ auth, request }) {
+      const user = auth?.user;
+      const isOnLoginPage =request.nextUrl?.pathname.startsWith("/")||
+        request.nextUrl?.pathname.startsWith("/login") ||
+        request.nextUrl?.pathname.startsWith("/register");
+      const isOnCartPage = request.nextUrl?.pathname.startsWith("/cart");
+      const isOnCartsPage = request.nextUrl?.pathname.startsWith("/carts");
 
-            if (isOnLoginPage && user) {
-                return Response.redirect(new URL("/carts", request.nextUrl));
-            }
-            if(isOnCartPage && !user)
-            {
-                return Response.redirect(new URL("/login", request.nextUrl));
-            }
-            // if (isOnLoginOrReg && user) {
-            //     return Response.redirect(new URL("/cart", request.nextUrl));
-            // }
-            if(isOnCartsPage && !user){
-                return Response.redirect(new URL("/login", request.nextUrl));
-            }
-            
-            return true;
-        }
-    }
+      if (isOnLoginPage && user) {
+        return Response.redirect(new URL("/carts", request.nextUrl));
+      }
+      if (isOnCartPage && !user) {
+        return Response.redirect(new URL("/", request.nextUrl));
+      }
+    //   if (isOnCartPage && !user) {
+    //     return Response.redirect(new URL("/login", request.nextUrl));
+    //   }
+      // if (isOnLoginOrReg && user) {
+      //     return Response.redirect(new URL("/cart", request.nextUrl));
+      // }
+      if (isOnCartsPage && !user) {
+        return Response.redirect(new URL("/login", request.nextUrl));
+      }
+
+      return true;
+    },
+  },
 };
-
-
-
-
