4ebb0f0daa3e369d753cb8512862d5e2ed86ada4
tsahi pahima <tsahi013@gmail.com>
2024-09-20 22:13:39 +0300

    .

--- src/app/api/shufersal/route.ts
+++ src/app/api/shufersal/route.ts
@@ -5,7 +5,7 @@ import { NextResponse } from 'next/server';
 
 export const GET = async (req:any) => {
   const { searchParams } = new URL(req.url);
-  const name = searchParams.get('query'); // מחלץ את הפרמטר 'query' מהכתובת
+  const name = searchParams.get('query') ??""; // מחלץ את הפרמטר 'query' מהכתובת
   console.log("🚀 ~ GET ~ name:", name); // מדפיס את השם
 
   try {
@@ -18,21 +18,3 @@ export const GET = async (req:any) => {
     return NextResponse.json({ error: 'Failed to fetch posts' }, { status: 500 });
   }
 };
-
-
-
-
-// export const GET = async (req: any) => {
-//   const name = req;
-//   console.log("🚀 ~ GET ~ name:", name)
-//   try {
-//     // const name = "עגבניה"; // Static query for now
-//     const response = await axios.get(`https://www.shufersal.co.il/online/he/search/results?q=${encodeURIComponent(name)}%3Arelevance&limit=10`);
-    
-//     // Only return the actual data, not the whole Axios response object
-//     return NextResponse.json(response.data);
-//   } catch (err) {
-//     console.error('Error fetching data:', err);
-//     return NextResponse.json({ error: 'Failed to fetch posts' }, { status: 500 });
-//   }
-// };
